09c484e8b37212ca2482a0768c3f6319afe1de4a
sharphurt <pavellukoanov679@gmail.com>
2021-05-12 16:45:57 +0500

    Changed way to transform figures

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,15 +17,14 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        public float Angle;
+        public Point Position;
+        
         public Form1()
         {
             DoubleBuffered = true;
-            
             InitializeComponent();
         }
-
-        public float Angle;
-        public Point Position;
 
         protected override void OnPaint(PaintEventArgs e)
         {
@@ -33,62 +32,52 @@ namespace WindowsFormsApp1
 
             g.FillRectangle(new SolidBrush(Color.Red), 0, 0, Width, Height);
 
-            var myPath1 = new GraphicsPath();
-            var path = new GraphicsPath(
-                new[] {new Point(0, 25), new Point(100, 25), new Point(60, 280), new Point(40, 280)},
-                new byte[] {1, 1, 1, 1});
-            myPath1.AddPath(path, false);
-
-            var myPath2 = new GraphicsPath();
-            myPath2.AddClosedCurve(new[] {new Point(5, 30), new Point(50, 0), new Point(95, 30)}, 1);
-
-            var unitedRegion = new Region(new Rectangle(0, 0, Width * 3, Height * 3));
+            var path = new GraphicsPath();
+            path.AddArc(0, 0, 100, 25, 180, 180);
+            path.AddLines(new[] {new Point(100, 25), new Point(60, 280), new Point(40, 280), new Point(0, 25)});
+            
+            var ellipsePath = new GraphicsPath();
+            ellipsePath.AddEllipse(new Rectangle(new Point(50 - 100, 280 - 80), new Size(200, 200)));
+            
+            var unitedRegion = new Region(new Rectangle(0, 0, Width, Height));
 
             var translateMatrix = new Matrix();
-            translateMatrix.Translate(Width, Height);
-
-            myPath1.Transform(translateMatrix);
-            myPath2.Transform(translateMatrix);
-
-
-            unitedRegion.Exclude(myPath1);
-            unitedRegion.Exclude(myPath2);
-
-            unitedRegion.Translate(-Width / 2 - 50 + Position.X, -Height / 2 - 100 + Position.Y);
-
+            translateMatrix.Translate(Width / 2 - 50 + Position.X, Height / 2 - 300 + Position.Y);
+            
             var rotationMatrix = new Matrix();
-            rotationMatrix.RotateAt(Angle, new PointF(Width / 2f + Position.X, Height + Position.Y));
-
-            unitedRegion.Transform(rotationMatrix);
-
+            rotationMatrix.RotateAt(Angle, new PointF(Width / 2 + Position.X, Height / 2 + Position.Y));
+            
+            path.Transform(translateMatrix);
+            path.Transform(rotationMatrix);
+            
+            ellipsePath.Transform(translateMatrix);
+            ellipsePath.Transform(rotationMatrix);
+            
+            unitedRegion.Exclude(path);
+            unitedRegion.Exclude(ellipsePath);
+            
             e.Graphics.FillRegion(Brushes.Black, unitedRegion);
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
         {
             if (e.KeyCode == Keys.W)
-                Position += new Size(0, -1);
+                Position += new Size(0, -10);
             if (e.KeyCode == Keys.S)
-                Position += new Size(0, 1);
+                Position += new Size(0, 10);
             if (e.KeyCode == Keys.D)
-                Position += new Size(1, 0);
+                Position += new Size(10, 0);
             if (e.KeyCode == Keys.A)
-                Position += new Size(-1, 0);
+                Position += new Size(-10, 0);
             Invalidate();
         }
 
         protected override void OnMouseMove(MouseEventArgs e)
         {
-            float x = (e.X - Width / 2) - Position.X;
-            float y = (e.Y - Height) - Position.Y;
+            float x = e.X - (Width / 2 + Position.X);
+            float y = e.Y - (Height / 2 + Position.Y);
 
-            var vectorAngle = (float) (Math.Atan(y / x) * 180 / Math.PI);
-            
-            if (vectorAngle < 0)
-                Angle = 90 + vectorAngle;
-            else
-                Angle = vectorAngle - 90;
-            
+            Angle = (float) (Math.Atan2(y, x) * 180 / Math.PI) + 90;
             Invalidate();
         }
     }
